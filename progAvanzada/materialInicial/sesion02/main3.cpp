41f0c8319ef144e006146409b7303f6fcfd113ce
DRMiguelAR <repepinoptero@gmail.com>
2026-03-30 14:55:21 -0600

    Add files via upload

---
+++ progAvanzada/materialInicial/sesion02/main3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string nombre;
+    cout<<"Introduce tu nombre: ";
+    cin>> nombre;
+    cout<<"Hello " << nombre << endl;
+
+}
